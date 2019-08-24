98ce10c30df81b70e413a527f6f6ba38ac1ef873
Mathuba <Mathuba58@gmail.com>
2019-08-24 22:01:40 +0200

    Added functions to build trie tree

--- trie_matching.py
+++ trie_matching.py
@@ -2,17 +2,38 @@
 import sys
 
 NA = -1
+GENOMICS = ['A', 'C', 'G', 'T']
 
 class Node:
 	def __init__ (self):
+		self.value = None
 		self.next = [NA] * 4
+
+
+def insert(root, gene_sequence):
+	current_node = root
+	for symbol in gene_sequence:
+		symbol_index = GENOMICS.index(symbol)
+		if current_node.next[symbol_index] == NA:
+			current_node.next[symbol_index] = Node()
+		current_node = current_node.next[symbol_index]
+	current_node.value = gene_sequence
+
+def build_trie(patterns):
+	tree = Node()
+	for pattern in patterns:
+		insert(tree, pattern)
+	return tree
 
 def solve (text, n, patterns):
 	result = []
 
-	// write your code here
+	trie = build_trie(patterns)
+	print("trie next at least: ", trie.next)
 
 	return result
+
+
 
 text = sys.stdin.readline ().strip ()
 n = int (sys.stdin.readline ().strip ())
